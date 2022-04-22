5b4912530922167ca04a046caa680cf8f1c67f8d
lemoscaio <caio_lemos94@hotmail.com>
2022-04-22 06:03:21 -0300

    Adiciona outra URL na lista de matches

--- content.js
+++ content.js
@@ -1,93 +1,96 @@
 let listaRestaurantes = [];
-inicializarFiltro();
+const categorias = [
+    "Africana",
+    "Alemã",
+    "Árabe",
+    "Argentina",
+    "Asiática",
+    "Açaí",
+    "Baiana",
+    "Bebidas",
+    "Brasileira",
+    "Cafeteria",
+    "Carnes",
+    "Casa de Sucos",
+    "Chinesa",
+    "Colombiana",
+    "Congelados Fit",
+    "Congelados",
+    "Contemporânea",
+    "Conveniência",
+    "Coreana",
+    "Cozinha rápida",
+    "Crepe",
+    "Doces & Bolos",
+    "Espanhola",
+    "Francesa",
+    "Frangos",
+    "Frutos do mar",
+    "Gaúcha",
+    "Grega",
+    "Hambúrguer",
+    "Indiana",
+    "Italiana",
+    "Japonesa",
+    "Lanches",
+    "Marmita",
+    "Marroquina",
+    "Mediterrânea",
+    "Mercado",
+    "Mexicana",
+    "Mineira",
+    "Nordestina",
+    "Padaria",
+    "Panqueca",
+    "Paranaense",
+    "Pastel",
+    "Peixes",
+    "Peruana",
+    "Pizza",
+    "Portuguesa",
+    "Presentes",
+    "Salgados",
+    "Saudável",
+    "Sopas & Caldos",
+    "Sorvetes",
+    "Tailandesa",
+    "Tapioca",
+    "Típica do Norte",
+    "Variada",
+    "Vegana",
+    "Vegetariana",
+    "Xis",
+    "Yakisoba",
+];
 
-const raizPagina = document.querySelector("[data-reactroot]");
-raizPagina.addEventListener("DOMNodeInserted", inicializarFiltro);
+verificarCarregamentoPagina();
+
+function verificarCarregamentoPagina() {
+    const raizPagina = document.querySelector("html");
+    raizPagina.addEventListener("DOMNodeInserted", inicializarFiltro);
+}
 
 function inicializarFiltro() {
-    let restaurantesWrapper = document.querySelector(
-        '[data-card-name="MERCHANT_LIST_V2"]'
-    );
+    const intervaloChecagem = setInterval(() => {
+        let restaurantesWrapper = document.querySelector(
+            '[data-card-name="MERCHANT_LIST_V2"]'
+        );
 
-    const divFiltro = document.querySelector(".div-extensao");
+        const divFiltro = document.querySelector(".div-extensao");
 
-    if (restaurantesWrapper && !divFiltro) {
-        const categorias = [
-            "Africana",
-            "Alemã",
-            "Árabe",
-            "Argentina",
-            "Asiática",
-            "Açaí",
-            "Baiana",
-            "Bebidas",
-            "Brasileira",
-            "Cafeteria",
-            "Carnes",
-            "Casa de Sucos",
-            "Chinesa",
-            "Colombiana",
-            "Congelados Fit",
-            "Congelados",
-            "Contemporânea",
-            "Conveniência",
-            "Coreana",
-            "Cozinha rápida",
-            "Crepe",
-            "Doces & Bolos",
-            "Espanhola",
-            "Francesa",
-            "Frangos",
-            "Frutos do mar",
-            "Gaúcha",
-            "Grega",
-            "Hambúrguer",
-            "Indiana",
-            "Italiana",
-            "Japonesa",
-            "Lanches",
-            "Marmita",
-            "Marroquina",
-            "Mediterrânea",
-            "Mercado",
-            "Mexicana",
-            "Mineira",
-            "Nordestina",
-            "Padaria",
-            "Panqueca",
-            "Paranaense",
-            "Pastel",
-            "Peixes",
-            "Peruana",
-            "Pizza",
-            "Portuguesa",
-            "Presentes",
-            "Salgados",
-            "Saudável",
-            "Sopas & Caldos",
-            "Sorvetes",
-            "Tailandesa",
-            "Tapioca",
-            "Típica do Norte",
-            "Variada",
-            "Vegana",
-            "Vegetariana",
-            "Xis",
-            "Yakisoba",
-        ];
+        if (restaurantesWrapper && !divFiltro) {
+            console.log("adicionei");
 
-        listaRestaurantes = pegarListaRestaurantes();
-        restaurantesWrapper.addEventListener("DOMNodeInserted", checarFiltro);
-
-        adicionarInputFiltro(categorias, restaurantesWrapper);
-    }
-    else {
-        setTimeout(() => {
-            restaurantesWrapper = document.querySelector(
-                '[data-card-name="MERCHANT_LIST_V2"]'
+            listaRestaurantes = pegarListaRestaurantes();
+            restaurantesWrapper.addEventListener(
+                "DOMNodeInserted",
+                checarFiltro
             );
-        }, 2000)
-    }
+
+            adicionarInputFiltro(categorias, restaurantesWrapper);
+            clearInterval(intervaloChecagem);
+        }
+    }, 1000);
 }
 
 function pegarListaRestaurantes() {
